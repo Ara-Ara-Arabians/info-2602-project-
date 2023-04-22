e6b24f93b65a174380bf780189d0f9631f6e306e
Chefboyadee <abdullaaahali@gmail.com>
2023-04-22 01:04:05 +0000

    models

--- App/models/user.py
+++ App/models/user.py
@@ -1,3 +1,4 @@
+
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from App.database import db
@@ -6,6 +7,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    map_url = db.Column(db.String(200), nullable=False)#if we dont api then this redundant
+    schedule = db.Column(db.String(200), nullable=False)
+    fare = db.Column(db.Float, nullable=False)
+
+class Driver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.String(2000), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
 
     def __init__(self, username, password):
         self.username = username
