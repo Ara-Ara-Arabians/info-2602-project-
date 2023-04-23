639202c84f28fcdf7dd93af8cbd55f8b0ebe1802
nicholasr74 <nicholasramroop74@gmail.com>
2023-04-23 02:37:34 +0000

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@ import click, pytest, sys
 from flask import Flask
 from flask.cli import with_appcontext, AppGroup
 
-#from App import App, db, Route
 from App.database import db, get_migrate
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users )
